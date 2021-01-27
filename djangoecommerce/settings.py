49e7979f3a18170d1906b3a8025fa819ddff80e8
ricardofacincani <ricardo.facincani@outlook.com>
2021-01-27 11:38:13 -0300

    alteração do arquivo settings.py para inclusão do Heroku

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = "/static/"
 
+# Configuração para o Heroku
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
 
